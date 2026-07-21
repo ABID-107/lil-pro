43d02b67d8545fbb9056fa90c3338b66e31c1a99
ABID-107 <abidurrahman5000@gmail.com>
2026-07-21 14:17:42 +0600

    last update

--- index.js
+++ index.js
@@ -11,11 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     fs.readdir('./files', (err, files) => {
-        if (err) {
-            return res.render('index', { files: [] });
-        }
         res.render('index', { files: files });
     });
+});
+app.get('/readMore', (req, res) => {
+    res.send(req.body);
+});
+app.get('/delete/:filename', (req, res) => {
+    fs.unlink(`./files/${req.params.filename}`, (err) => {
+        res.redirect('/');
+    });
+});
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description,(err) => {res.redirect('/')});
 });
 
 
